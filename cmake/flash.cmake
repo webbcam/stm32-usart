7248777126cb4e190e7334af3b5d5273d5a5ac63
Cameron Webb <cjwebb@cams-mbp.local>
2020-09-09 00:12:31 -0700

    only use flashcmd for debug builds

--- cmake/flash.cmake
+++ cmake/flash.cmake
@@ -1,7 +1,9 @@
 # CMAKE flashing tool
 
-if (NOT FLASHCMD)
-    find_path(STFLASH_DIR "st-flash" REQUIRED)
-    set(FLASHCMD ${STFLASH_DIR}/st-flash write ${PROJECT_NAME}.bin 0x8000000)
+if (CMAKE_BUILD_TYPE STREQUAL "DEBUG")
+    if (NOT FLASHCMD)
+        find_path(STFLASH_DIR "st-flash" REQUIRED)
+        set(FLASHCMD ${STFLASH_DIR}/st-flash write ${PROJECT_NAME}.bin 0x8000000)
+    endif()
 endif()
 
