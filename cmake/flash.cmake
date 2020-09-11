03c40a32e80bf898ce5a63fb78d9557d8525db9f
Cameron Webb <webbjcam@gmail.com>
2020-09-10 22:32:50 -0700

    added reset to stflash command

--- cmake/flash.cmake
+++ cmake/flash.cmake
@@ -3,7 +3,7 @@
 if (CMAKE_BUILD_TYPE STREQUAL "DEBUG")
     if (NOT FLASHCMD)
         find_path(STFLASH_DIR "st-flash" REQUIRED)
-        set(FLASHCMD ${STFLASH_DIR}/st-flash write ${PROJECT_NAME}.bin 0x8000000)
+        set(FLASHCMD ${STFLASH_DIR}/st-flash --reset write ${PROJECT_NAME}.bin 0x8000000)
     endif()
 endif()
 
